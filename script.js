8ea3afe02434baf54dd6c5b69bef9ca99b3a57d2
sujoy0908 <sujoysamanta0908@gmail.com>
2025-01-22 11:02:50 +0530

    updted

--- script.js
+++ script.js
@@ -53,5 +53,5 @@ function plusSlides(n) {
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].style.display = "block  ";
 }
